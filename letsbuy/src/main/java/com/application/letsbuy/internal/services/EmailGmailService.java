d9cd5fad2b99a3945849fe6bf3a870f9f6872ad5
kawan-goncalves <kawangferreira027@gmail.com>
2023-03-16 11:59:13 -0300

    feature(EmailController): arrumando respostas http e adicionando html para boas-vindas

--- letsbuy/src/main/java/com/application/letsbuy/internal/services/EmailGmailService.java
+++ letsbuy/src/main/java/com/application/letsbuy/internal/services/EmailGmailService.java
@@ -24,32 +24,77 @@ public class EmailGmailService extends HtmlEmail implements Sendable {
 
 
     @Override
-    public Boolean sendWelcome(String receiverEmail, String receiverName) {
+    public Boolean sendWelcome(String receiverName,String receiverEmail) {
         try {
             setSubject("Vamos começar a comprar e vender juntos na LetsBuy!");
-            URL url = new URL("https://i.imgur.com/NNQnocY.png");
-            String cid = embed(url, "LetsBuy");
-
-            setHtmlMsg("<h1><img src=\""+cid+"\" alt=\"\" width=\"129\" height=\"48\" /></h1>\n" +
-                    "<p>Ol&aacute;, "+receiverName+"!</p>\n" +
-                    "<p><span class=\"x_ContentPasted0\">Estamos muito felizes em t&ecirc;-lo(a) a bordo da LetsBuy, a plataforma que conecta pessoas para comprar e&nbsp;</span><span class=\"x_ContentPasted0\">vender produtos usados!</span></p>\n" +
-                    "<p>&nbsp;</p>\n" +
-                    "<div>\n" +
-                    "<p><span class=\"x_ContentPasted0\">Para que voc&ecirc; possa aproveitar todas as oportunidades que a LetsBuy oferece, &eacute; necess&aacute;rio confirmar o&nbsp;</span><span class=\"x_ContentPasted0\">seu endere&ccedil;o de e-mail. &Eacute; super f&aacute;cil e r&aacute;pido, e voc&ecirc; j&aacute; estar&aacute; pronto(a) para come&ccedil;ar a explorar&nbsp;</span><span class=\"x_ContentPasted0\">todas as ofertas dispon&iacute;veis.</span></p>\n" +
-                    "<p>&nbsp;</p>\n" +
-                    "<p font'><span class=\"x_ContentPasted0\"'>Para confirmar o seu e-mail, por favor, clique no bot&atilde;o m&aacute;gico que preparamos para voc&ecirc;:</span></p>\n" +
-                    "<p>&nbsp;</p>\n" +
-                    "<p>At&eacute; breve!</p>"+
-                    "<div><span class=\"x_ContentPasted0\">&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp;</span></div>\n" +
-                    "<div>&nbsp;</div>\n" +
-                    "<div style='display: flex;justify-content: center;align-items: center'>"+
-                    "<button style='display: flex;justify-content: center;align-items: center;max-width: 300px;width: 100%;height: 50px;color: #FFFF;background-color: #F14866;font-family: 'Poppins';font-size: 16px;font-weight: 500;border-radius: 5px;margin: 50px 0;'>Confirmar Email</button>"+
-                    "<div>&nbsp;</div>\n"+
-                    "</div>"+
-                    "<div>&nbsp;</div>\n"+
-                    "<div style='display: flex;justify-content: center;border-top: 1px solid #E6E1DD;padding: 10px 0;'>" +
-                    "<p style=\"font-family: 'sans-serif';font-size: 12px;font-weight: 500;color: #8F8F8F;text-align: center;\">LetsBuy - &copy; Todos os direitos reservados</p>" +
-                    "</div>"+
+            setHtmlMsg("<div style=\"width:100%;font-family:arial,'helvetica neue',helvetica,sans-serif;padding:0;Margin:0\">\n" +
+                    "<div style=\"background-color:#f6f6f6\">\n" +
+                    "<table width=\"100%\" cellspacing=\"0\" cellpadding=\"0\" style=\"border-collapse:collapse;border-spacing:0px;padding:0;Margin:0;width:100%;height:100%;background-repeat:repeat;background-position:center top;background-color:#f6f6f6\">\n" +
+                    "<tbody><tr>\n" +
+                    "<td valign=\"top\" style=\"padding:0;Margin:0\">\n" +
+                    "<table class=\"m_6229933818003390889es-header\" cellspacing=\"0\" cellpadding=\"0\" align=\"center\" style=\"border-collapse:collapse;border-spacing:0px;table-layout:fixed!important;width:100%;background-color:transparent;background-repeat:repeat;background-position:center top\">\n" +
+                    "<tbody><tr>\n" +
+                    "<td align=\"center\" style=\"padding:0;Margin:0\">\n" +
+                    "<table class=\"m_6229933818003390889es-header-body\" cellspacing=\"0\" cellpadding=\"0\" bgcolor=\"#ffffff\" align=\"center\" style=\"border-collapse:collapse;border-spacing:0px;background-color:#ffffff;width:600px\">\n" +
+                    "<tbody><tr>\n" +
+                    "<td align=\"left\" style=\"padding:0;Margin:0;padding-top:20px;padding-left:20px;padding-right:20px\">\n" +
+                    "<table cellspacing=\"0\" cellpadding=\"0\" width=\"100%\" style=\"border-collapse:collapse;border-spacing:0px\">\n" +
+                    "<tbody><tr>\n" +
+                    "<td class=\"m_6229933818003390889es-m-p0r\" valign=\"top\" align=\"center\" style=\"padding:0;Margin:0;width:560px\">\n" +
+                    "<table width=\"100%\" cellspacing=\"0\" cellpadding=\"0\" role=\"presentation\" style=\"border-collapse:collapse;border-spacing:0px\">\n" +
+                    "<tbody><tr>\n" +
+                    "<td align=\"center\" style=\"padding:0;Margin:0;font-size:0px\"><img class=\"m_6229933818003390889adapt-img CToWUd\" src=\"https://ci6.googleusercontent.com/proxy/u37faxLaK8dAOIJECx4Mgl2GUOgA_63Td4ouDUBW0bYgTX6Y4JusshPRjpBT6T6k-Zs=s0-d-e1-ft#https://i.imgur.com/NNQnocY.png\" alt=\"\" style=\"display:block;border:0;outline:none;text-decoration:none\" height=\"59\" width=\"159\" data-bit=\"iit\"></td>\n" +
+                    "</tr>\n" +
+                    "</tbody></table></td>\n" +
+                    "</tr>\n" +
+                    "</tbody></table></td>\n" +
+                    "</tr>\n" +
+                    "</tbody></table></td>\n" +
+                    "</tr>\n" +
+                    "</tbody></table>\n" +
+                    "<table class=\"m_6229933818003390889es-content\" cellspacing=\"0\" cellpadding=\"0\" align=\"center\" style=\"border-collapse:collapse;border-spacing:0px;table-layout:fixed!important;width:100%\">\n" +
+                    "<tbody><tr>\n" +
+                    "<td align=\"center\" style=\"padding:0;Margin:0\">\n" +
+                    "<table class=\"m_6229933818003390889es-content-body\" cellspacing=\"0\" cellpadding=\"0\" bgcolor=\"#ffffff\" align=\"center\" style=\"border-collapse:collapse;border-spacing:0px;background-color:#ffffff;width:600px\">\n" +
+                    "<tbody><tr>\n" +
+                    "<td align=\"left\" style=\"padding:0;Margin:0;padding-top:20px;padding-right:20px;padding-left:30px\">\n" +
+                    "<table width=\"100%\" cellspacing=\"0\" cellpadding=\"0\" style=\"border-collapse:collapse;border-spacing:0px\">\n" +
+                    "<tbody><tr>\n" +
+                    "<td valign=\"top\" align=\"center\" style=\"padding:0;Margin:0;width:550px\">\n" +
+                    "<table width=\"100%\" cellspacing=\"0\" cellpadding=\"0\" role=\"presentation\" style=\"border-collapse:collapse;border-spacing:0px\">\n" +
+                    "<tbody><tr>\n" +
+                    "<td align=\"left\" bgcolor=\"#ffffff\" style=\"padding:0;Margin:0\"><p style=\"Margin:0;font-family:arial,'helvetica neue',helvetica,sans-serif;line-height:21px;color:#333333;font-size:14px\">Olá, Kawan!</p><p style=\"Margin:0;font-family:arial,'helvetica neue',helvetica,sans-serif;line-height:21px;color:#333333;font-size:14px\">Estamos muito felizes em tê-lo(a) a bordo da LetsBuy, a plataforma que conecta pessoas para comprar e&nbsp;vender produtos usados!</p><p style=\"Margin:0;font-family:arial,'helvetica neue',helvetica,sans-serif;line-height:21px;color:#333333;font-size:14px\"><br></p><p style=\"Margin:0;font-family:arial,'helvetica neue',helvetica,sans-serif;line-height:21px;color:#333333;font-size:14px\">Para que você possa aproveitar todas as oportunidades que a LetsBuy oferece, é necessário confirmar o&nbsp;seu endereço de e-mail. É super fácil e rápido, e você já estará pronto(a) para começar a explorar&nbsp;todas as ofertas disponíveis.</p><p style=\"Margin:0;font-family:arial,'helvetica neue',helvetica,sans-serif;line-height:21px;color:#333333;font-size:14px\"><br></p><p style=\"Margin:0;font-family:arial,'helvetica neue',helvetica,sans-serif;line-height:21px;color:#333333;font-size:14px\">Para confirmar o seu e-mail, por favor, clique no botão mágico que preparamos para você:</p><p style=\"Margin:0;font-family:arial,'helvetica neue',helvetica,sans-serif;line-height:21px;color:#333333;font-size:14px\"><br></p><p style=\"Margin:0;font-family:arial,'helvetica neue',helvetica,sans-serif;line-height:21px;color:#333333;font-size:14px\">Até breve!</p></td>\n" +
+                    "</tr>\n" +
+                    "</tbody></table></td>\n" +
+                    "</tr>\n" +
+                    "</tbody></table></td>\n" +
+                    "</tr>\n" +
+                    "</tbody></table></td>\n" +
+                    "</tr>\n" +
+                    "</tbody></table>\n" +
+                    "<table class=\"m_6229933818003390889es-footer\" cellspacing=\"0\" cellpadding=\"0\" align=\"center\" style=\"border-collapse:collapse;border-spacing:0px;table-layout:fixed!important;width:100%;background-color:transparent;background-repeat:repeat;background-position:center top\">\n" +
+                    "<tbody><tr>\n" +
+                    "<td align=\"center\" style=\"padding:0;Margin:0\">\n" +
+                    "<table class=\"m_6229933818003390889es-footer-body\" cellspacing=\"0\" cellpadding=\"0\" bgcolor=\"#ffffff\" align=\"center\" style=\"border-collapse:collapse;border-spacing:0px;background-color:#ffffff;width:600px\">\n" +
+                    "<tbody><tr>\n" +
+                    "<td align=\"left\" style=\"Margin:0;padding-top:20px;padding-bottom:20px;padding-left:20px;padding-right:20px\">\n" +
+                    "<table cellspacing=\"0\" cellpadding=\"0\" width=\"100%\" style=\"border-collapse:collapse;border-spacing:0px\">\n" +
+                    "<tbody><tr>\n" +
+                    "<td align=\"left\" style=\"padding:0;Margin:0;width:560px\">\n" +
+                    "<table width=\"100%\" cellspacing=\"0\" cellpadding=\"0\" role=\"presentation\" style=\"border-collapse:collapse;border-spacing:0px\">\n" +
+                    "<tbody><tr>\n" +
+                    "<td align=\"center\" style=\"padding:0;Margin:0\"><span class=\"m_6229933818003390889es-button-border\" style=\"background:#f14866;border-width:0px 0px 2px 0px;display:inline-block;border-radius:5px;width:auto\"><a class=\"m_6229933818003390889es-button\" style=\"text-decoration:none;color:#ffffff;font-size:18px;display:inline-block;background:#f14866;border-radius:5px;font-family:verdana,geneva,sans-serif;font-weight:normal;font-style:normal;line-height:22px;width:auto;text-align:center;padding:10px 20px 10px 20px;border-color:#f14866\">Confirmar email</a></span></td>\n" +
+                    "</tr>\n" +
+                    "</tbody></table></td>\n" +
+                    "</tr>\n" +
+                    "</tbody></table></td>\n" +
+                    "</tr>\n" +
+                    "</tbody></table></td>\n" +
+                    "</tr>\n" +
+                    "</tbody></table></td>\n" +
+                    "</tr>\n" +
+                    "</tbody></table>\n" +
+                    "</div>\n" +
                     "</div>");
             setTextMsg("Seu servidor de e-mail não suporta mensagem HTML");
 
@@ -63,11 +108,39 @@ public class EmailGmailService extends HtmlEmail implements Sendable {
 
     @Override
     public Boolean sendChangePassword(String receiverEmail, String receiverName) {
-        return null;
+
+        try {
+            setSubject("Solicitação de troca de senha LetsBuy!");
+            URL url = new URL("https://i.imgur.com/NNQnocY.png");
+            String cid = embed(url, "LetsBuy");
+
+            setHtmlMsg("HTML para outlook");
+            setTextMsg("Seu servidor de e-mail não suporta mensagem HTML");
+
+            addTo(receiverEmail);
+            send();
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
     }
 
     @Override
     public Boolean sendSaleConfirmation(String receiverEmail, String receiverName) {
-        return null;
+
+        try {
+            setSubject("Parabéns, você vendeu na LetsBuy!");
+            URL url = new URL("https://i.imgur.com/NNQnocY.png");
+            String cid = embed(url, "LetsBuy");
+
+            setHtmlMsg("HTML para outlook");
+            setTextMsg("Seu servidor de e-mail não suporta mensagem HTML");
+
+            addTo(receiverEmail);
+            send();
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
     }
 }

199baaa51d8f76256ac3422209696bc16772aa23
Gustavo Rezende <gustavo.silva@sptech.school>
2023-04-11 17:33:32 -0300

    ASCII logo added

--- letsbuy/src/main/java/com/application/letsbuy/LetsbuyApplication.java
+++ letsbuy/src/main/java/com/application/letsbuy/LetsbuyApplication.java
@@ -1,5 +1,6 @@
 package com.application.letsbuy;
 
+import com.application.letsbuy.internal.utils.AscIILogo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class LetsbuyApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(LetsbuyApplication.class, args);
+		System.out.println("\n \n" + AscIILogo.logo);
 	}
 
 }

27572ca490e0004e7100d20ba65ebe8b402f5b1f
Leonardo Nacagawa Silva <leonardonacasilva@gmail.com>
2023-06-11 22:52:55 -0300

    fix bank-account

--- src/main/java/com/application/letsbuy/internal/entities/BankAccount.java
+++ src/main/java/com/application/letsbuy/internal/entities/BankAccount.java
@@ -1,5 +1,6 @@
 package com.application.letsbuy.internal.entities;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -21,6 +22,7 @@ public class BankAccount {
 
     @ManyToOne
     @JoinColumn(name = "user_id")
+    @JsonBackReference
     private User user;
 
     @Column

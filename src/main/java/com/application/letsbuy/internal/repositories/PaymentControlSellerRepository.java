ebcb183ab60a0049dc018aefe887322992f7953f
kawan-goncalves <kawangferreira027@gmail.com>
2023-06-14 00:48:46 -0300

    endpoints de minhas compras

--- src/main/java/com/application/letsbuy/internal/repositories/PaymentControlSellerRepository.java
+++ src/main/java/com/application/letsbuy/internal/repositories/PaymentControlSellerRepository.java
@@ -6,10 +6,13 @@ import com.application.letsbuy.internal.entities.PaymentUserAdvertisement;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface PaymentControlSellerRepository extends JpaRepository<PaymentControllSeller, Long> {
 
     Optional<PaymentControllSeller> findByPaymentUserAdvertisementAdversimentId(Long idAdversiment);
+
+    List<PaymentControllSeller> findByPaymentUserAdvertisementBuyerId(Long idBuyer);
 }
